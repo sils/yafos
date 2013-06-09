88935adf2f61f2ccb9c3d17074a6817ee5f211f5
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@bc89edb3-f490-2326-732d-79c633a96469>
2013-06-09 11:49:26 +0000

    Made the toolchain installer more intelligent and clean.

--- cc/toolchain.sh
+++ cc/toolchain.sh
@@ -10,6 +10,19 @@
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 
+
+abort()
+{
+	echo "Toolchain builder was interrupted. If you want to continue the build please do it manually."
+	cleanup
+}
+
+cleanup()
+{
+	echo "Cleaning working directory."
+	rm -f -R $wdir
+}
+
 echo "/-------------------------------------------\\"
 echo "|                                           |"
 echo "|                 YAFOS CCI                 |"
@@ -18,62 +31,101 @@ echo "|        (c) 2013 by Matthias Riegler       |"
 echo "|                                           |"
 echo "\\-------------------------------------------/"
 
-
 echo
-echo "Welcome to this Q&D tool." # if this ever works...
+echo "Welcome to the YAFOS Toolchain installer."
 
+if [ "$(id -u)" == "0" ]
+then
+	echo "WARNING: This script is not intended to run as root!\
+Press any key to continue or CTRL + C to abort.
+Consider running this script with a developement user."
+	read
+fi
 
-echo "Creating working directory."
-mkdir ~/cc
-cd ~/cc
+until ping -W 5 -c 1 google.com >>/dev/null;
+do
+	echo "No internet connection present! You'll be given the output of 'ip link'."
+	echo "If you have an ethernet connection you may execute dhcpcd <thedevice>."
+	echo "If you are to establish a wireless connection try running wifi-menu."
+	echo "An easy alternative would be your GUI."
+	echo "Type exit to return to this script."
+	ip link
+	echo -n "Do you want to be put into a bash to set up a connection? [y/n]"
+	read answer
+	if [ "$answer" != "n" ]
+	then
+		bash
+	else
+		exit 1
+	fi
+done
 
-echo "getting crosstool-ng"
-## first get the crosstool-ng toolchain builder
+wdir="$HOME/cc"
+while [ -d "$wdir" ]
+do
+	c="c"
+	wdir="$wdir$c"
+done
+
+trap abort SIGINT
+
+echo "Creating working directory ($wdir)..."
+mkdir $wdir
+cd $wdir
+echo "Done."
+
+echo "Downloading toolchain builder (crosstool-ng)..."
 wget http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.18.0.tar.bz2
-echo "done"
-echo "...now extracting source"
-##extract source
+echo "Done."
+
+echo "Extracting source..."
 tar xf crosstool-ng-1.18.0.tar.bz2
-echo "done"
+echo "Done."
 
 cd -
 
 ## copy patch
-cp ./cc/patch_makefile_recursion.patch ~/cc/crosstool-ng-1.18.0/
+cp ./cc/patch_makefile_recursion.patch $wdir/crosstool-ng-1.18.0/
 
 
 ## get into the source tree
-cd ~/cc/crosstool-ng-1.18.0
+cd $wdir/crosstool-ng-1.18.0
 
 ## patch
-echo "Patch Makefile.in"
+echo "Applying recursion patch..."
 patch -p1 < patch_makefile_recursion.patch
-echo "configure toolchain"
-## configure
+echo "Done."
+
+if [ -d "/tmp/ct-ng"]
+then
+	rm -R -f /tmp/ct-ng
+fi
+
+echo "Configure crosstool-ng..."
 ./configure --prefix=/tmp/ct-ng/
-echo "done"
-## compile
+echo "Done."
 
-echo "... install"
-
-make install
+echo "Install crosstool-ng..."
+make install -j4
+echo "Done."
 
 cd -
 
-mkdir -p ~/cc/toolchain 
+mkdir -p $wdir/toolchain 
 
 ## copy config file
-cp ./cc/CONFIG_YAFOS ~/cc/toolchain/.config
+cp cc/CONFIG_YAFOS $wdir/toolchain/.config
 
-cd ~/cc/toolchain
+cd $wdir/toolchain
 
 
 ##build toolchain
-echo "building toolchain, this process can take up to 3 houres!!"
+echo "Building toolchain...""
 /tmp/ct-ng/bin/ct-ng build
+echo "Done."
 
-echo "cleaning"
-#rm -R ~/cc
-echo "done"
+echo "Cleaning up..."
+cleanup
+echo "Done."
 
-
+echo "Toolchain built successfully! Enjoy the compilation!"
