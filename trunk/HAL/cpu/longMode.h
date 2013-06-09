394276065b60b3db7bf974b36ec226a8c1e24e86
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@bc89edb3-f490-2326-732d-79c633a96469>
2013-06-09 12:25:56 +0000

    Added new plain files, added roadmap to long mode ;) let's hope we make it.

---
+++ trunk/HAL/cpu/longMode.h
@@ -0,0 +1,19 @@
+/* HAL/cpu/longMode.h
+ * 
+ * TODO
+ * 
+ * Copyright (C) 2013 Lasse Schuirmann. All Rights Reserved.
+ * Written by Lasse Schuirmann (lasse.schuirmann@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ */
+
+#ifndef _LONGMODE_H
+#define _LONGMODE_H
+
+void initLongMode();
+
+#endif /* _MONGMODE_H */
