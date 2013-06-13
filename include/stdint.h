73f0e5ebe6094f35f76ea906e19c3338276b34b8
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@bc89edb3-f490-2326-732d-79c633a96469>
2013-06-13 05:54:14 +0000

    Minor changes.

--- include/stdint.h
+++ include/stdint.h
@@ -22,8 +22,10 @@ typedef short               int16_t;
 typedef unsigned short      uint16_t;
 typedef int                 int32_t;
 typedef unsigned            uint32_t;
+#if ARCH == 64
 typedef long long           int64_t;
 typedef unsigned long long  uint64_t;
+#endif
 
 // TODO least, fast, greatest
 
---
+++ kernel/mem/physical/pMem.c
@@ -0,0 +1,5 @@
+#include "pMem.h"
+
+#if ARCH == 32
+//static uint32_t bitmap[]//TODO find out, how large the ram is
+#endif
