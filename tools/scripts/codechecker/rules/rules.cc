f2b91cdc54ae47f9096e4dc6038cfbfe30385cb1
lasse.schuirmann@gmail.com <lasse.schuirmann@gmail.com@bc89edb3-f490-2326-732d-79c633a96469>
2013-06-17 11:16:39 +0000

    Wrote specification for codechecker. Added directories and so on.

---
+++ tools/scripts/codechecker/rules/rules.cc
@@ -0,0 +1,12 @@
+/* rules.cc
+ * 
+ * Contains rules for the codechecker.
+ *
+ * Copyright (C) 2013 Lasse Schuirmann. All Rights Reserved.
+ * Written by Lasse Schuirmann (lasse.schuirmann@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ */
